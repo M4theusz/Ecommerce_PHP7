8c904ef5d30dc302101be1ce28c41c4a722819b9
M4theusz <matheusvinnicios@hotmail.com>
2018-03-12 16:06:45 -0300

    Configurando Login

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php 
+session_start();
 
 require_once("vendor/autoload.php");
 
+use \Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -16,6 +18,8 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new Hcode\PageAdmin();
 
@@ -23,6 +27,32 @@ $app->get('/admin', function() {
 
 });
 
-$app->run();
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=> false,
+		"footer"=>false
+		]);
 
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout',function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+});
+
+$app->run();
  ?>
